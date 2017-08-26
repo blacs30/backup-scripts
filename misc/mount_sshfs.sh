ea20bcc9c6e173f5d1dab8544009578dd8c4145c
blacs30 <github@lisowski-development.com>
2017-08-26 18:17:38 +0200

    Add check for errors while doing a read test on the mount in case it is mount. In case of read error of a mounted share do an umount and mount again.
    Signed-off-by: blacs30 <github@lisowski-development.com>

--- misc/mount_sshfs.sh
+++ misc/mount_sshfs.sh
@@ -2,21 +2,55 @@
 # create a cron enry to call this script every x minutes / hours
 # 30/* * * * * /root/backup/mount_sshfs.sh
 
+# set -x
+
 check_cmd="$(mount | grep -c webbackup)"
 LOG="/root/mount_sshfs.log"
 HOST=$(hostname)
 MAIL_RECIPIENT=admin@example.com
+MOUNT_URL='sshfs webbackup@example.com:/mnt/data/webbackup'
+MOUNT_PATH=/root/webbackup
 
 ##
 ## Write output to logfile
 ##
 
-if [ "$check_cmd" -ne "1" ]; then
-   if sshfs webbackup@example.com:/mnt/data/webbackup /root/webbackup 2>/dev/null; then
+mount() {
+    if  ${MOUNT_URL} ${MOUNT_PATH} 2>/dev/null; then
+
         echo "webbackup successfully mounted again at $(date)." | tee $LOG
+        return 0
     else
+
         echo "webbackup not available, sending mail to admin." | tee $LOG
         mailx -a "From: \"$HOST\" <\"$HOST\">" -s "Mounting webbackup | ""$HOST" $MAIL_RECIPIENT < $LOG
+        exit 1
     fi
-fi
+}
 
+unmount() {
+    if ! umount ${MOUNT_PATH} 2>/dev/null; then
+
+        echo "webbackup could not be unmount at $(date)." | tee $LOG
+        return 1
+    fi
+}
+
+checkmount() {
+     if ! ls ${MOUNT_PATH}  > /dev/null 2>&1; then
+
+           echo "ERROR while reading from mounted drive at $(date)." | tee $LOG
+           echo "Trying to remount at $(date)." | tee $LOG
+           unmount
+           mount
+        fi
+}
+
+if [ "$check_cmd" -ne "1" ]; then
+
+  mount
+  checkmount
+else
+
+    checkmount
+fi
